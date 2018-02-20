423263744066d9cc4ed74fca25435e4c9a5727f3
Jed Long <jed.long@st-andrews.ac.uk>
2018-02-20 21:30:10 +0000

    Package attempt

---
+++ R/teamInfo.R
@@ -0,0 +1,12 @@
+#' Team information data
+#'
+#' Team information data to be linked to team ID.
+#' 
+#' Basic team information data, really to be used as an internal look up table for certain functions.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format An \code{data.frame} object providing basic linking info for each team ID.
+#' @name teamInfo
+#-----------------------------------
+NULL
